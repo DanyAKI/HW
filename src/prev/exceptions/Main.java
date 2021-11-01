6e7e091a65e3cbb4a4d2215d4c88d10f0cfc00cb
Dany <daniilakimov@bk.ru>
2021-11-01 18:39:11 +0300

    NEW ARRAY

--- src/prev/exceptions/Main.java
+++ src/prev/exceptions/Main.java
@@ -3,16 +3,19 @@ package prev.exceptions;
 public class Main {
     public static void main(String[] args) {
         String array[][] = new String[4][4];
-        String counter = "AD";
+        String [][] arr ={
+        {"1", "1", "1", "1"},
+        {"1", "1", "1", "1"},
+        {"1", "1", "1", "1"},
+        {"1", "t", "1", "1"}
+    };
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
-                array[i][j]= counter;
                 System.out.print(array[i][j]+ " ");
             }
             System.out.println();
-
             ArrayCalcValue sc = new ArrayCalcValue();
-            sc.calc(array);
+            sc.calc(arr);
 
 
         }
