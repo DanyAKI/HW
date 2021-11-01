8b125953d223f43d5d5cd4490576a7b05fd9a859
Dany <daniilakimov@bk.ru>
2021-11-01 18:58:36 +0300

    New array + new ArrayCalcValue

--- src/prev/exceptions/Main.java
+++ src/prev/exceptions/Main.java
@@ -7,20 +7,18 @@ public class Main {
         {"1", "1", "1", "1"},
         {"1", "1", "1", "1"},
         {"1", "1", "1", "1"},
-        {"1", "t", "1", "1"}
+        {"1", "1", "1", "1"}
     };
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                System.out.print(array[i][j]+ " ");
-            }
-            System.out.println();
+
             ArrayCalcValue sc = new ArrayCalcValue();
             sc.calc(arr);
 
 
+
+
         }
     }
-}
+
 
 
 
